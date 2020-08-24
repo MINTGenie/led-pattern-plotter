c43c39e70cbf1a4cc2931e7773dc9dca98103e86
MINT Genie <66748747+MINTGenie@users.noreply.github.com>
2020-08-24 23:59:55 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+basic.forever(function () {
+    for (let y = 0; y <= 4; y++) {
+        for (let x = 0; x <= y; x++) {
+            led.plot(x, y)
+            basic.pause(100)
+        }
+    }
+    basic.pause(1000)
+    basic.clearScreen()
+    for (let y = 0; y <= 4; y++) {
+        for (let x = 0; x <= y; x++) {
+            led.plot(x, 4 - y)
+            basic.pause(100)
+        }
+    }
+    basic.pause(1000)
+    basic.clearScreen()
+    for (let y = 0; y <= 4; y++) {
+        for (let x = 0; x <= y; x++) {
+            led.plot(4 - x, y)
+            basic.pause(100)
+        }
+    }
+    basic.pause(1000)
+    basic.clearScreen()
+    for (let y = 0; y <= 4; y++) {
+        for (let x = 0; x <= y; x++) {
+            led.plot(4 - x, 4 - y)
+            basic.pause(100)
+        }
+    }
+    basic.pause(1000)
+    basic.clearScreen()
+})
